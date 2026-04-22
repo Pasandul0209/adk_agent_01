4840d66b3fd6beb29a050bb55a09c16cbc36b0c0
Pasandul Kasthuri <nethmika86@gmail.com>
2026-04-22 17:29:16 +0530

    update

--- agent.py
+++ agent.py
@@ -3,6 +3,6 @@ from google.adk.agents import Agent
 root_agent = Agent(
     name="my_Assistant",
     model="gemini-3.1-flash-lite",
-    description="A helpful assistant.",
+    description="A helpful Assistant.",
     instruction="You are a helpful assistant. Answer the questions clearly.",
 )
